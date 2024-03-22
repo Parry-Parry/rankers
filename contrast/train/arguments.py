a104c829b140b782c8acce0b3de15fa5d17d4e9e
Andrew Parry <2393265p@student.gla.ac.uk>
2024-03-22 15:23:52 +0000

    final changes

--- contrast/train/arguments.py
+++ contrast/train/arguments.py
@@ -1,33 +1,11 @@
-import argparse
 from transformers import TrainingArguments
 from typing import Union
 import torch.nn as nn
-
-def main(triples_file : str, 
-        teacher_file : str,
-        dataset_name : str, 
-        out_dir : str, 
-        model_name_or_path : str = 'google/electra-base-discriminator',
-        val_file : str = None,
-        max_epochs : int = 1, 
-        batch_size : int = 16, 
-        num_negatives : int = 1,
-        lr : float = 0.00001, 
-        grad_accum : int = 1,
-        warmup_steps : int = 0,
-        min_train_steps : int = 50000,
-        wandb_project=None,
-        mode : str = 'std',
-        early_patience : str = 30,
-        early_check : str = 4000,
-        rank : int = None,
-        fp16 : bool = False,):
-    pass
     
 class ContrastArguments(TrainingArguments):
     def __init__(self, 
                  loss_fn : Union[nn.Module, callable], 
-                 mode : str, 
+                 mode : str = None, 
                  num_negatives : int = 1,
                  margin : int = 1,
                  **kwargs):
