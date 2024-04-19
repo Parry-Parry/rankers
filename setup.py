b5ebcdb24da83aa2471b56d6333954b288c31653
Andrew Parry <a.parry.1@research.gla.ac.uk>
2024-04-19 15:54:59 +0100

    silly

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="contrast",
+    version="0.0.1",
+    author="Andrew Parry",
+    description="A framework for training and evaluating neural IR models.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+)
