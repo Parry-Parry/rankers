b2ed2c334f87f875e4723079f77595e1fde69395
Andrew Parry <a.parry.1@research.gla.ac.uk>
2024-04-23 10:06:16 +0100

    remove silly margin stuff

--- contrast/train/arguments.py
+++ contrast/train/arguments.py
@@ -7,10 +7,8 @@ class ContrastArguments(TrainingArguments):
                  loss_fn : Union[nn.Module, callable], 
                  mode : str = None, 
                  num_negatives : int = 1,
-                 margin : int = 1,
                  **kwargs):
         self.loss_fn = loss_fn
         self.mode = mode
         self.num_negatives = num_negatives
-        self.margin = margin
         super().__init__(**kwargs)
