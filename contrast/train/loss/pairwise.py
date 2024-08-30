c8e4314cad65eb77eff1476ae5837abd3af6c939
Andrew <2393265p@student.gla.ac.uk>
2024-08-30 11:27:40 +0100

    fixed

--- contrast/train/loss/pairwise.py
+++ contrast/train/loss/pairwise.py
@@ -45,8 +45,6 @@ class LCELoss(BaseLoss):
             labels = labels.argmax(dim=1)
         else:
             labels = torch.zeros(pred.size(0), dtype=torch.long, device=pred.device)
-        print(pred.size())
-        print(labels.size())
         return F.cross_entropy(pred, labels, reduction=self.reduction)
 
 
