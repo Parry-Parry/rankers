1a186243d86d975a20922eb6ea6a4ab12df65a40
“Andrew <a.parry.1@research.gla.ac.uk>
2024-11-12 16:05:05 +0000

    version bump for release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="rankers",
-    version="0.0.3",
+    version="0.0.4",
     author="Andrew Parry",
     description="A framework for training and evaluating neural IR models.",
     long_description=long_description,
