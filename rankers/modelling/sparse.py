b3908f17f8b7d7f90c792d3c2669ac5bec7888aa
“Andrew <a.parry.1@research.gla.ac.uk>
2024-11-14 09:15:08 +0000

    fix sparse

--- rankers/modelling/sparse.py
+++ rankers/modelling/sparse.py
@@ -1,27 +1,68 @@
 from rankers.modelling.dot import Pooler
 import torch
-from transformers import AutoModel, AutoConfig, AutoModel, PreTrainedModel, PreTrainedTokenizer
+from transformers import AutoModel, AutoConfig, PreTrainedModel, PreTrainedTokenizer, AutoModelForMaskedLM
 from .dot import DotConfig, Dot
 
 class SparseConfig(DotConfig):
     model_type = "Sparse"
-    def __init__(self, model_name_or_path: str = 'bert-base-uncased', pooling_type='cls', inbatch_loss=None, model_tied=True, use_pooler=False, pooler_dim_in=768, pooler_dim_out=768, pooler_tied=True, **kwargs):
+    def __init__(self, 
+                 model_name_or_path: str = 'bert-base-uncased', 
+                 query_processing : str = None,
+                 doc_processing : str = None,
+                 pooling_type='cls', 
+                 inbatch_loss=None, 
+                 model_tied=True, 
+                 use_pooler=False, 
+                 pooler_dim_in=768, 
+                 pooler_dim_out=768, 
+                 pooler_tied=True, **kwargs):
         super().__init__(model_name_or_path, pooling_type, inbatch_loss, model_tied, use_pooler, pooler_dim_in, pooler_dim_out, pooler_tied, **kwargs)
+        self.query_processing = query_processing
+        self.doc_processing = doc_processing
 
     @classmethod
-    def from_pretrained(cls, model_name_or_path: str = 'bert-base-uncased', pooling_type='cls', inbatch_loss=None, model_tied=True, use_pooler=False, pooler_dim_in=768, pooler_dim_out=768, pooler_tied=True) -> 'SparseConfig':
-        return cls(model_name_or_path, pooling_type, inbatch_loss, model_tied, use_pooler, pooler_dim_in, pooler_dim_out, pooler_tied)
-
+    def from_pretrained(cls, 
+                        model_name_or_path: str = 'bert-base-uncased', 
+                        query_processing : str = None,
+                        doc_processing : str = None,
+                        pooling_type='cls', 
+                        inbatch_loss=None, 
+                        model_tied=True, 
+                        use_pooler=False, 
+                        pooler_dim_in=768, 
+                        pooler_dim_out=768, 
+                        pooler_tied=True) -> 'SparseConfig':
+        config = super().from_pretrained(model_name_or_path, pooling_type, inbatch_loss, model_tied, use_pooler, pooler_dim_in, pooler_dim_out, pooler_tied)
+        config.query_processing = query_processing
+        config.doc_processing = doc_processing
+        return config
+    
+def splade_max(outputs, mask):
+    outputs = outputs.logits
+    relu = torch.nn.ReLU(inplace=False)
+    values, _ = torch.max(torch.log(1 + relu(outputs)) * mask.unsqueeze(-1), dim=1)
+    return values
+    
 class Sparse(Dot):
     model_type = "Sparse"
+    architecture_class = AutoModelForMaskedLM
     transformer_class = None
 
     def __init__(self, model: PreTrainedModel, tokenizer: PreTrainedTokenizer, config: DotConfig, model_d: PreTrainedModel = None, pooler: Pooler = None):
         super().__init__(model, tokenizer, config, model_d, pooler)
 
+        self.query_processing = splade_max if config.query_processing == 'splade_max' else lambda x : x.logits
+        self.doc_processing = splade_max if config.doc_processing == 'splade_max' else lambda x : x.logits
+
         from .pyterrier.sparse import SparseTransformer
         self.transformer_class = SparseTransformer
     
+    def _encode_d(self, **text):
+        return self.doc_processing(self.model_d(**text))
+    
+    def _encode_q(self, **text):
+        return self.query_processing(self.model(**text))
+        
     def forward(self, 
                 loss = None, 
                 queries = None, 
