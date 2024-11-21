c27d639a53c04b7a33ceaa280734b20ff616b161
“Andrew <a.parry.1@research.gla.ac.uk>
2024-11-21 19:18:47 +0000

    hmm

--- rankers/train/loss/__init__.py
+++ rankers/train/loss/__init__.py
@@ -131,50 +131,7 @@ def register_loss(name):
 
 _import_structure = {}
 
-
-try:
-    if not is_torch_available():
-        raise OptionalDependencyNotAvailable()
-except OptionalDependencyNotAvailable:
-    pass
-else:
-    _import_structure["loss"] = [
-        "BaseLoss",
-    ]
-
-try:
-    if not is_flax_available():
-        raise OptionalDependencyNotAvailable()
-except OptionalDependencyNotAvailable:
-    pass
-else:
-    _import_structure["flax_loss"] = [
-        "FlaxBaseLoss",
-    ]
-
-if TYPE_CHECKING:
-    try:
-        if not is_torch_available():
-            raise OptionalDependencyNotAvailable()
-    except OptionalDependencyNotAvailable:
-        pass
-    else:
-        from .torch import BaseLoss
-
-    try:
-        if not is_flax_available():
-            raise OptionalDependencyNotAvailable()
-    except OptionalDependencyNotAvailable:
-        pass
-    else:
-        from .flax import FlaxBaseLoss
-else:
-    import sys
-
-    sys.modules[__name__] = _LazyModule(
-        __name__, globals()["__file__"], _import_structure, module_spec=__spec__
-    )
-from .._optional import is_torch_available, is_flax_available
+from ..._optional import is_torch_available, is_flax_available
 from transformers.utils import _LazyModule, OptionalDependencyNotAvailable
 from typing import TYPE_CHECKING
 
