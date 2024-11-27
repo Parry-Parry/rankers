73229d647f84ffd7301afd1e8f4a68c67ebd4199
“Andrew <a.parry.1@research.gla.ac.uk>
2024-11-27 13:08:24 +0000

    fix pointwise

--- rankers/train/loss/flax/pointwise.py
+++ rankers/train/loss/flax/pointwise.py
@@ -1,5 +1,7 @@
 from . import FlaxBaseLoss
 from ..util import register_loss
+import jax.numpy as jnp
+import optax.losses as L
 
 
 @register_loss("flax_flax_pointwise_mse")
@@ -8,10 +10,10 @@ class PointwiseMSELoss(FlaxBaseLoss):
 
     name = "PointwiseMSE"
 
-    def forward(self, pred: Tensor, labels: Tensor) -> Tensor:
+    def forward(self, pred: jnp.Array, labels: jnp.Array) -> jnp.Array:
         flattened_pred = pred.view(-1)
         flattened_labels = labels.view(-1)
-        return F.mse_loss(flattened_pred, flattened_labels, reduction=self.reduction)
+        return self._reduce(L.squared_error(flattened_pred, flattened_labels))
 
 
 __all__ = [
