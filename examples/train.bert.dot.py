403cee05b10dba641f11a54a13524061a87093cb
“Andrew <a.parry.1@research.gla.ac.uk>
2024-12-29 14:00:13 +0000

    Minor update to example

--- examples/train.bert.dot.py
+++ examples/train.bert.dot.py
@@ -30,12 +30,14 @@ def main():
         pooling_type=model_args.pooling_type,
         model_tied=model_args.model_tied,
         use_pooler=model_args.use_pooler,
+        inbatch_loss=model_args.inbatch_loss,
     )
     model = Dot.from_pretrained(model_args.model_name_or_path, config=model_config)
 
     dataset = TrainingDataset(
         data_args.training_data,
-        ir_dataset=data_args.ir_dataset,
+        corpus=data_args.ir_dataset,
+        teacher_file=data_args.teacher_file,
         group_size=data_args.group_size,
         use_positive=data_args.use_positive,
     )
