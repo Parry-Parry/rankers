44a5c426b991e8fad2707ff91111a72b2b5b2ec8
“Andrew <a.parry.1@research.gla.ac.uk>
2025-03-31 13:37:11 +0100

    add conversion

---
+++ utilities/jsonl_to_lmdb.py
@@ -0,0 +1,25 @@
+import lmdb
+import json
+from fire import Fire
+
+
+def jsonl_to_lmdb(jsonl_path, lmdb_path):
+    """Convert JSONL triplets to LMDB"""
+    triplet_env = lmdb.open(lmdb_path, map_size=10**9)  # Triplets
+
+    with triplet_env.begin(write=True) as txn:
+        with open(jsonl_path, "r", encoding="utf-8") as f:
+            for i, line in enumerate(f):
+                idx = str(i).encode()
+                try:
+                    _ = json.loads(line)
+                except json.JSONDecodeError:
+                    print(f"Skipping line {i} due to JSON decoding error")
+                    continue
+                txn.put(idx, line.encode())
+
+    return f"Converted {jsonl_path} to {lmdb_path} with {i+1} records."
+
+
+if __name__ == "__main__":
+    Fire(jsonl_to_lmdb)
